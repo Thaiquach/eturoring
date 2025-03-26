0f88b48cba762f9957ea77d972920cbfff9b81eb
Lowkey.Lowky <139440439+huyentrang04012003@users.noreply.github.com>
2025-03-26 12:10:19 +0700

    add classService.js file

---
+++ src/api/classService.js
@@ -0,0 +1,18 @@
+// src/api/classService.js
+import axios from 'axios';
+
+const CLASS_API_URL = 'https://dummyjson.com/products';
+
+export default {
+  getClasses() {
+    return axios.get(CLASS_API_URL);
+  },
+
+  createClass(newClass) {
+    return axios.post(`${CLASS_API_URL}/add`, newClass);
+  },
+
+  updateClass(id, payload) {
+    return axios.put(`https://dummyjson.com/products/${id}`, payload)
+  }
+};
