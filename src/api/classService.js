be8d015870d385a36d15a73cb6d8a1c3731d3bcc
Thaiquach <139450404+Thaiquach@users.noreply.github.com>
2025-03-31 16:21:10 +0700

    ClassCRUD

--- src/api/classService.js
+++ src/api/classService.js
@@ -1,18 +1,57 @@
-// src/api/classService.js
 import axios from 'axios';
 
-const CLASS_API_URL = 'https://dummyjson.com/products';
+const BASE_URL = 'https://localhost:7050/api';
 
 export default {
-  getClasses() {
-    return axios.get(CLASS_API_URL);
+  // Lấy toàn bộ danh sách student
+  getAllStudents() {
+    const token = localStorage.getItem('token');
+    return axios.get(`${BASE_URL}/profile/students`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
   },
 
-  createClass(newClass) {
-    return axios.post(`${CLASS_API_URL}/add`, newClass);
+  // Gửi dữ liệu tạo lớp học mới
+  createClass(classData) {
+    const token = localStorage.getItem('token');
+    return axios.post(`${BASE_URL}/Class/create-class`, classData, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
   },
 
-  updateClass(id, payload) {
-    return axios.put(`https://dummyjson.com/products/${id}`, payload)
+  // Lấy danh sách tutor (đính kèm token)
+  getAllTutors() {
+    const token = localStorage.getItem('token');
+    return axios.get(`${BASE_URL}/profile/tutors`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  },
+
+  // Lấy danh sách subject
+  getAllSubjects() {
+    return axios.get(`${BASE_URL}/Subjects/get-all-subjects`);
+  },
+  getAllClasses() {
+    const token = localStorage.getItem('token');
+    return axios.get(`${BASE_URL}/Class/get-all-classes`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  },
+  updateClass(id, classData) {
+    const token = localStorage.getItem('token');
+    return axios.put(`${BASE_URL}/Class/update-class/${id}`, classData, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
   }
+  
 };
